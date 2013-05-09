56779e29b97122d65e2d766ee71c5abdfc7e8687
Alex Egan <aegan@andrew.cmu.edu>
2013-05-09 19:01:05 -0400

    tried a few things that did not work. Fixed them so now it works the same as before. Could have checked out an earlier commit but where is the fun in that

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,9 +1,7 @@
 # encoding: utf-8
-
 class PhotoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -29,7 +27,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  # process :resize_to_fit => [200, 300]
   #
   # def scale(width, height)
   #   # do something
